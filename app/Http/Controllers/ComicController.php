0c8d676b63fafa1d86f6a90a011dfe857de5944b
Marco Rinaldi <marcorinaldi98@libero.it>
2023-05-18 14:13:17 +0200

    BONUS aggiunti errori personalizzati in italiano

--- app/Http/Controllers/ComicController.php
+++ app/Http/Controllers/ComicController.php
@@ -36,15 +36,29 @@ class ComicController extends Controller
      */
     public function store(Request $request)
     {
-        $request->validate([
-            'title' => 'required|max:100',
-            'description' => 'nullable|max:65535',
-            'thumb' => 'nullable|max:255',
-            'price' => 'nullable|max:10',
-            'series' => 'required|max:100',
-            'sale_date' => 'nullable|max:20',
-            'type' => 'required|max:20',
-        ]);
+        $request->validate(
+            [
+                'title' => 'required|max:100',
+                'description' => 'nullable|max:65535',
+                'thumb' => 'nullable|max:255',
+                'price' => 'nullable|max:10',
+                'series' => 'required|max:100',
+                'sale_date' => 'nullable|max:20',
+                'type' => 'required|max:20'
+            ],
+            [
+                'title.required' => 'il titolo è un campo obbligatorio',
+                'title.max' => 'il titolo deve avere al massimo :max caratteri',
+                'description.max' => 'la descizione deve avere al massimo :max caratteri',
+                'thumb.max' => 'la URL dell\' immagine deve avere al massimo :max caratteri',
+                'price.max' => 'il prezzo deve avere al massimo :max caratteri',
+                'series.required' => 'la serie è un campo obbligatorio',
+                'series.max' => 'la serie deve avere al massimo :max caratteri',
+                'sale_date.max' => 'la data di uscita deve avere al massimo :max caratteri',
+                'type.required' => 'il genere è un campo obbligatorio',
+                'type.max' => 'il genere deve avere al massimo :max caratteri'
+            ]
+        );
 
         $form_data = $request->all();
 
@@ -88,15 +102,29 @@ class ComicController extends Controller
      */
     public function update(Request $request, Comic $comic)
     {
-        $request->validate([
-            'title' => 'required|max:100',
-            'description' => 'nullable|max:65535',
-            'thumb' => 'nullable|max:255',
-            'price' => 'nullable|max:10',
-            'series' => 'required|max:100',
-            'sale_date' => 'nullable|max:20',
-            'type' => 'required|max:20',
-        ]);
+        $request->validate(
+            [
+                'title' => 'required|max:100',
+                'description' => 'nullable|max:65535',
+                'thumb' => 'nullable|max:255',
+                'price' => 'nullable|max:10',
+                'series' => 'required|max:100',
+                'sale_date' => 'nullable|max:20',
+                'type' => 'required|max:20'
+            ],
+            [
+                'title.required' => 'il titolo è un campo obbligatorio',
+                'title.max' => 'il titolo deve avere al massimo :max caratteri',
+                'description.max' => 'la descizione deve avere al massimo :max caratteri',
+                'thumb.max' => 'la URL dell\' immagine deve avere al massimo :max caratteri',
+                'price.max' => 'il prezzo deve avere al massimo :max caratteri',
+                'series.required' => 'la serie è un campo obbligatorio',
+                'series.max' => 'la serie deve avere al massimo :max caratteri',
+                'sale_date.max' => 'la data di uscita deve avere al massimo :max caratteri',
+                'type.required' => 'il genere è un campo obbligatorio',
+                'type.max' => 'il genere deve avere al massimo :max caratteri'
+            ]
+        );
 
         $form_data = $request->all();
 
